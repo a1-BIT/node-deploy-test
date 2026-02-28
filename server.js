41516f790e2ac6e79aa3e9fe4db91508d9747641
a1-BIT <ayadav23198@gmail.com>
2026-02-28 22:16:16 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.json({ message: "Hello World!", status: 200 });
+});
+
+app.get("/about", (req, res) => {
+    res.json({ message: "About Page", status: 200 });
+});
+
+app.get("/contact", (req, res) => {
+    res.json({ message: "Contact Page", status: 200 });
+});
+
+app.get("/products", (req, res) => {
+    res.json({ message: "Products Page", status: 200 });
+});
+
+app.use((req, res) => {
+    res.status(404).json({ message: "Page Not Found", status: 404 });
+});
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
+
